b37b5e2ab9e0f0c78195b7c365f369fdceb02761
X-TECH CORP <zabsonrejacques30@gmail.com>
2025-08-30 21:24:35 +0000

    now

---
+++ payment.js
@@ -0,0 +1,50 @@
+Informations sur l'application
+Masquer les clés api
+Nom de l'applicationXenon-e-shop
+DescriptionVotre boutique en ligne de confiance pour tous vos besoins.
+URL du site Webhttps://xenon-e-shop.onrender.com/#
+Statut de l'applicationActivée
+ServicesPayin Payout
+Envoie de Facture de paiementActivé
+Clé Principale
+nVs3hWJb-L2oP-v7d9-Ggs0-wyiGMKe61udF   
+Clés API de Test
+Clé Publique
+test_public_BUyO1nun77TgmHEHAORpWyWf2yA   
+Clé Privée
+test_private_hEJzz7aqD8NEoArnhIlSLCut3Xp   
+Token
+yqkn6u4pG2kbalr2qC0u   
+Clés API de Production
+Clé Publique
+live_public_ufDqY1yKzttmv6BM6nuKDq4Q26p   
+Clé Privée
+live_private_ksWjEfJWcEVXiwcXV16j5A2gHln   
+Token
+m0cF1vCgpn4umShCg7Ut   
+Méthodes de paiement autorisées
+Carte Bancaire
+CARD
+Sénégal
+ORANGE MONEY SENEGAL WIZALL SENEGAL FREE MONEY SENEGAL WAVE SENEGAL
+Bénin
+MOOV BENIN MTN BENIN
+Burkina Faso
+ORANGE MONEY BURKINA MOOV BURKINA FASO
+Cote d'ivoire
+ORANGE MONEY CI MTN CI MOOV CI Wave CI
+Mali
+ORANGE MONEY MALI
+Togo
+T MONEY TOGO MOOV TOGO
+
+Paiement à la livraison (PAL)
+EtatDésactivé
+Paiement ET Redistribution (PER) / Déboursement
+EtatActivé
+Instant Payment Notification (IPN)
+Endpoint IPN
+EtatDésactivé
+Actions
+
+Retour à la page précédente
